b55383998a0de15268375d4d4c1b402a369d1568
Mitchell Scobell <mitchscobell@gmail.com>
2021-08-10 18:37:32 -0500

    updated repo to have query

--- src/github-repository.js
+++ src/github-repository.js
@@ -4,18 +4,36 @@ const fetch = require("node-fetch");
 
 // import config file
 nconf.argv().env().file({ file: "./appconfig.json" });
-
 const apiConfiguration = nconf.get("apiConfiguration");
-
-console.log(apiConfiguration);
 
 /// write some code that will retrieve every pull request
 /// for the Ramda organization using the Github web API
 /// and store the results in memory
 const getPullRequests = function () {
-  return "";
+  // query for pulling info
+  const query = `
+    query {
+      repository(owner:"${apiConfiguration.owner}", name:"${apiConfiguration.name}") {
+        issues(states:CLOSED) {
+          totalCount
+        }
+      }
+    }`;
+
+  fetch("https://api.github.com/graphql", {
+    method: "POST",
+    body: JSON.stringify({ query }),
+    headers: {
+      Authorization: `Bearer ${apiConfiguration.accessToken}`,
+    },
+  })
+    .then((res) => res.text())
+    .then((body) => console.log(body)) // {"data":{"repository":{"issues":{"totalCount":247}}}}
+    .catch((error) => console.error(error));
 };
 
-//TODO abstract away the method calls
+getPullRequests();
+
+// TODO abstract away the method calls
 
 // TODO possibly caching results
