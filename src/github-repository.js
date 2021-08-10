24286d3723e46b4554a214b12b1f75febf555c8a
Mitchell Scobell <mitchscobell@gmail.com>
2021-08-10 18:10:07 -0500

    initialized api calls

--- src/github-repository.js
+++ src/github-repository.js
@@ -1,14 +1,17 @@
-const getConnection = function () {
-  // we'll build a tool to analyze pull request traffic for a Github organization.
-};
+const fetch = require("node-fetch");
+const accessToken = "your_access_token_from_github";
+const owner = "isaacs"; // TODO swap for Ramda organization
+const name = "github";
+const url = "https://api.github.com/graphql";
 
-/// write some code that will retrieve every pull request 
-/// for the Ramda organization using the Github web API 
+/// write some code that will retrieve every pull request
+/// for the Ramda organization using the Github web API
 /// and store the results in memory
-const getURL = function () {
+const getPullRequests = function () {
   return "";
 };
 
-const getPullRequests = function {
-    return '';
-}
+
+//TODO abstract away the method calls
+
+// TODO possibly caching results
