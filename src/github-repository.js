99aa6225d76bd80a83a980feef54cfd49a0ca954
Mitchell Scobell <mitchscobell@gmail.com>
2021-08-10 18:02:13 -0500

    starting stubbing out the repo

---
+++ src/github-repository.js
@@ -0,0 +1,14 @@
+const getConnection = function () {
+  // we'll build a tool to analyze pull request traffic for a Github organization.
+};
+
+/// write some code that will retrieve every pull request 
+/// for the Ramda organization using the Github web API 
+/// and store the results in memory
+const getURL = function () {
+  return "";
+};
+
+const getPullRequests = function {
+    return '';
+}
