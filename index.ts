c612f853c28de756c099991eec0afd67ca342d79
Mitchell Scobell <mitchscobell@gmail.com>
2021-08-11 21:14:41 -0500

    added some zazz

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 import { GithubService } from "./services/github-service";
+const chalk = require("chalk");
 
 async function main(): Promise<void> {
+  // initialize the console
+  const log = console.log;
+  console.clear();
+
   const org: string = "ramda";
+
+  log(
+    chalk.white.bgRed("Loading data for ") +
+      chalk.white.bgGreen.bold(org) +
+      chalk.white.bgRed(" organization") + '\n'
+  );
+
   const service = new GithubService();
 
   let repositories = await service.getRepositoriesForOrg(org);
@@ -9,13 +21,22 @@ async function main(): Promise<void> {
   let amountOfPullRequests = 0;
 
   for (let repo of repositories) {
-    console.log(`Repository Name: ${repo.name}`);
+    log(chalk.white.bgBlue("Repository Name:") + chalk.cyan(` ${repo.name}`));
     let repoInfo = await service.getPullRequestsForOrgAndRepo(org, repo.name);
-    console.log(`Number of Pull Requests: ${repoInfo.length}\n`);
+    log(
+      chalk.blue("Number of Pull Requests:") +
+        chalk.magenta(` ${repoInfo.length}`)
+    );
     amountOfPullRequests += repoInfo.length;
   }
 
-  console.log(`\nTotal Number of Pull Requests for ${org} organization: ${amountOfPullRequests}\n\n`);
+  log(
+    chalk.red(`\nTotal Number of Pull Requests for `) +
+      chalk.black.bgGreen.bold(`${org}`) +
+      chalk.red(` organization: `) +
+      chalk.green(`${amountOfPullRequests}`) +
+      `\n`
+  );
 }
 
 main();
